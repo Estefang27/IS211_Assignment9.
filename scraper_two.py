07f88c6bc95db0f6ac746a531fb8a78637aed420
Estefang27 <123829468+Estefang27@users.noreply.github.com>
2025-04-15 00:00:57 -0400

    Update scraper_two.py

--- scraper_two.py
+++ scraper_two.py
@@ -1,3 +1,32 @@
 
+from bs4 import BeautifulSoup
+import requests
+
+
+url = "https://finance.yahoo.com/quote/AAPL"
+
+def get_apple_stock_data():
+   
+    response = requests.get(url)
+    
+   
+    if response.status_code != 200:
+        print(f"Failed to retrieve data: HTTP Status Code {response.status_code}")
+        return
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    try:
+        current_price = soup.find('fin-streamer', {'data-field': 'regularMarketPrice'}).text.strip()
+        previous_close = soup.find('fin-streamer', {'data-field': 'regularMarketPreviousClose'}).text.strip()
+        market_cap = soup.find('fin-streamer', {'data-field': 'marketCap'}).text.strip()
+
+        print(f"Current Price of AAPL: ${current_price}")
+        print(f"Previous Close: ${previous_close}")
+        print(f"Market Cap: {market_cap}")
+
+    except AttributeError:
+        print("Failed to retrieve some of the data.")
+
 if __name__ == "__main__":
-    print("Running scrapper two...")
+    get_apple_stock_data()
